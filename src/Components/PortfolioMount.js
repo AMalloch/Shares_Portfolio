3365e9fd1a99aff7388d94e6754b7dd07d1e6b3f
Angus Malloch <angusbass@hotmail.com>
2019-04-16 18:14:58 +0100

    Remove unused code from this file.

--- src/Components/PortfolioMount.js
+++ src/Components/PortfolioMount.js
@@ -11,11 +11,6 @@ const PortfolioMount = props => {
     );
   });
 
-  const handleChangeSector = event => {
-    let sector = event.target.value;
-    props.onSelectedBySector(sector);
-  };
-
   const handleChange = event => {
     let index = event.target.value;
     props.handleStockSelected(index);
@@ -47,11 +42,5 @@ const PortfolioMount = props => {
     </React.Fragment>
   );
 };
-
-// select a stock to buy, save it by index to a selectedStock array
-// pass that through props back to the marketstock and populate
-// the form with hidden input fields except company name, volume and
-// price. Post the new object to the portfolio collection in the db
-//
 
 export default PortfolioMount;
